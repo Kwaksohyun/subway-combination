5f1d627bae29714a1cef80d5002056c2f0e1f29d
Kwaksohyun <73460505+Kwaksohyun@users.noreply.github.com>
2024-03-05 21:22:12 +0900

    Feat: MenuItem hover시 디자인 추가

--- src/Routes/Sandwich.tsx
+++ src/Routes/Sandwich.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import data from "../data.json";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const MenuListWrap = styled.div`
     background-color: #f2f2f2;
@@ -84,15 +85,6 @@ const ProductListWrap = styled.div`
     }
 `;
 
-const MenuItem = styled(motion.li)`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 370px;
-    height: 330px;
-    background-color: #fff;
-`;
-
 const Img = styled.img`
     width: 280px;
     height: 200px;
@@ -100,7 +92,6 @@ const Img = styled.img`
         margin-top: 15px;
     }
 `;
-
 
 const Title = styled.strong`
     font-size: 22px;
@@ -117,6 +108,76 @@ const Calorie = styled.span`
     font-size: 18px;
     font-weight: 800;
     color: ${(props) => props.theme.yellow.lighter};
+`;
+
+const Summary = styled.div`
+    line-height: normal;
+    word-break: keep-all;
+    text-align: center;
+    color: #c3e698;
+    font-size: 15px;
+    width: 310px;
+    opacity: 0;
+    position: absolute;
+    top: 150px;
+`;
+
+const ViewBtn = styled.svg`
+    width: 52px;
+    height: 52px;
+    border-radius: 100%;
+    padding: 7px;
+    background-color: ${(props) => props.theme.yellow.lighter};
+    fill: #fff;
+    opacity: 0;
+    position: absolute;
+    bottom: 28px;
+    transform: scale(0.7);
+`;
+
+const MenuItem = styled(motion.li)`
+    width: 370px;
+    height: 330px;
+    background-color: #fff;
+    position: relative;
+    > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    &:hover {
+        background-color: ${(props) => props.theme.green.lighter};
+        z-index: 5;
+        ${Img}, ${Calorie} {
+            opacity: 0;
+        }
+        ${Title} {
+            color: #fff;
+            position: absolute;
+            top: 135px;
+            transform: translateY(-80px);
+            transition: transform 0.4s;
+        }
+        ${EngTitle} {
+            color: #fff;
+            position: absolute;
+            top: 175px;
+            transform: translateY(-80px);
+            transition: transform 0.4s;
+        }
+        ${Summary} {
+            opacity: 1;
+            position: absolute;
+            top: 203px;
+            transform: translateY(-80px);
+            transition: transform 0.4s 0.1s;    // property-name | duration | delay
+        }
+        ${ViewBtn} {
+            opacity: 1;
+            transform: scale(1);
+            transition: transform 0.3s 0.1s;
+        }
+    }
 `;
 
 // interface ISandwichInfo {
@@ -182,11 +243,23 @@ function Sandwich() {
                     <ul>
                         <AnimatePresence mode="wait">
                             {sandwichTab[activeTab].dataList.map(sandwichInfo => (
-                                <MenuItem variants={menuItemVariants} initial="invisible" animate="visible" exit="exit" key={`${sandwichTab[activeTab].name}_${sandwichInfo.id}`}>
-                                    <Img className={activeTab===4 ? "topping" : ""} alt={sandwichInfo["eng_title"]+"_img"} src={`${process.env.PUBLIC_URL}/${sandwichInfo.img}`} />
-                                    <Title>{sandwichInfo.title}</Title>
-                                    <EngTitle>{sandwichInfo.eng_title}</EngTitle>
-                                    {activeTab!==4 && <Calorie>{sandwichInfo.calorie}</Calorie>}
+                                <MenuItem variants={menuItemVariants} initial="invisible" animate="visible" exit="exit" 
+                                    key={`${sandwichTab[activeTab].name}_${sandwichInfo.id}`}>
+                                    {/* 메뉴소개 상세페이지로 이동 */}
+                                    <Link to={`/menuView/sandwich/?menuItemIdx=${sandwichInfo.id}`}>
+                                        <Img className={activeTab===4 ? "topping" : ""} alt={sandwichInfo["eng_title"]+"_img"} src={`${process.env.PUBLIC_URL}/${sandwichInfo.img}`} />
+                                        <Title>{sandwichInfo.title}</Title>
+                                        <EngTitle>{sandwichInfo.eng_title}</EngTitle>
+                                        {activeTab!==4 && <Calorie>{sandwichInfo.calorie}</Calorie>}
+                                        <Summary>
+                                            <p>{sandwichInfo.summary.split(' \n ').map(i => <>{i}<br/></>)}</p>
+                                        </Summary>
+                                        {activeTab!==4 && (
+                                            <ViewBtn enable-background="new 0 0 32 32" version="1.1" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" id="XMLID_223_"/>
+                                            </ViewBtn>
+                                        )}
+                                    </Link>
                                 </MenuItem>
                             ))}
                         </AnimatePresence>
